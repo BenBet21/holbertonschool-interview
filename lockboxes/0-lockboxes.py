f0eb8faed82e0c6e010f148452ff34bee410e226
BenBet21 <benoit.beti@gmail.com>
2025-04-16 09:49:16 +0200

    0. Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = set([0])  # On commence avec la boîte 0 ouverte
+    keys = [0]         # Liste des boîtes à visiter (on commence par la 0)
+
+    while keys:
+        current = keys.pop()  # On prend une boîte à ouvrir
+        for key in boxes[current]:
+            if key < len(boxes) and key not in opened:
+                opened.add(key) # On ajoute la boîte à visiter si elle n'est pas déjà ouverte
+                keys.append(key) #On la met dans keys pour l’ouvrir plus tard
+                
+    # On vérifie si toutes les boîtes sont ouvertes
+    return len(opened) == len(boxes)
