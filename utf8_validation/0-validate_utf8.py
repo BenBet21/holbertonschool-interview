5067b01f0fad5474d2220749bef901d0fb2d9aab
BenBet21 <benoit.beti@gmail.com>
2025-06-06 10:46:05 +0200

    0. UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
+    """
+    vérifie sur la liste d'entier est bien encodée en UTF-8 encoding.
+    """
     n_bytes = 0
 
     for num in data:
-        # Garder seulement les 8 bits de poids faible
         byte = num & 0xFF
 
         if n_bytes == 0:
-            # Compter le nombre de bits à 1 au début du byte
             masque = 0b10000000
             while masque & byte:
                 n_bytes += 1
                 masque >>= 1
 
             if n_bytes == 0:
-                continue  # 1-byte char
+                continue
 
             if n_bytes == 1 or n_bytes > 4:
-                return False  # UTF-8 ne commence jamais par 10 ou plus de 4
+                return False
 
-            n_bytes -= 1  # car le premier byte est déjà lu
+            n_bytes -= 1
         else:
-            # Doit être un byte de continuation => 10xxxxxx
             if not (byte & 0b10000000 and not (byte & 0b01000000)):
                 return False
             n_bytes -= 1
